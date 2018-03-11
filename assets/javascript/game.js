ff65db655ab028e17a8282f5e90e7ed06c7d341a
Ronald Deguzman <bootcampcoding.ronnie@gmail.com>
2018-03-11 00:02:41 -0800

    Sundayedits

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+// Set function to give command that any key pressed will be stored
+
+// script{
+//     var typedLetter = {}
+// }
+
+// depending on what category choosen, create a boolean with the letters of that item of the category to be true with the correct key strokes
+
+
+// True booleans, will be stored, then pushed into a a div with letter indexes for the corresponding letters of the correct word to be guess. 
